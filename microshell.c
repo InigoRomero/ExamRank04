6fb5cdb1a1b59d9fe1af791261c6391e9a05698d
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2020-11-11 20:02:19 +0100

    Clean logic for less lines

--- microshell.c
+++ microshell.c
@@ -2,88 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct	s_command{
+typedef struct s_command{
 	int type;
 	char **argv;
-	char **argvaux;
+	char **argvaux; 
 }				t_command;
 
-int ft_strlen(char *src)
+int ft_strlen(char *str)
 {
 	int i = 0;
-	while (src[i])
+	while (str[i] != '\0')
 		i++;
 	return (i);
 }
 
-char *ft_strdup(char *str)
+char *ft_strdup(char *src)
 {
-	int i = 0;
 	char *dest;
-	if (!(dest = malloc(ft_strlen(str) + 1)))
+	int i = 0;
+	if (!(dest = malloc(sizeof(char *) * ft_strlen(src) + 1)))
 		return (NULL);
-	while (str[i] != '\0')
+	while (src[i] != '\0')
 	{
-		dest[i] = str[i];
+		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = '\0';
+	dest[i] = 0;
 	return (dest);
 }
 
-int show_error(char *src)
+int show_error(char *str)
 {
-	if (src)
-		write (2, src, (size_t)ft_strlen(src));
+	if (str)
+		write(2, str, (size_t)ft_strlen(str));
 	return (0);
 }
 
-int exit_fatal()
+int fatal_error()
 {
 	show_error("error: fatal\n");
 	exit(EXIT_FAILURE);
-	return(EXIT_FAILURE);
+	return (EXIT_FAILURE);
 }
 
-int parse_command(t_command *stru, int i)
+int ft_parse_command(t_command *stru, int i)
 {
 	int x = 0, aux = i;
-	while (stru->argvaux[i] && strcmp(stru->argvaux[i], ";") && strcmp(stru->argvaux[i], "|"))
+	while (stru->argvaux[i] && strcmp(stru->argvaux[i], "|") && strcmp(stru->argvaux[i], ";"))
 		i++;
 	if (!(stru->argv = (char**)malloc(sizeof(char**) * (i - aux + 1))))
-		return (-1);
+		return (0);
 	while (x + aux < i)
 	{
 		stru->argv[x] = ft_strdup(stru->argvaux[x + aux]);
 		x++;
 	}
 	stru->argv[x] = 0;
-	if (stru->argvaux[i] && strcmp(";", stru->argvaux[i]) == 0)
+	if (stru->argvaux[i] && !strcmp(stru->argvaux[i], ";"))
 		stru->type = 1;
-	if (stru->argvaux[i] && strcmp( "|", stru->argvaux[i]) == 0)
+	if (stru->argvaux[i] && !strcmp(stru->argvaux[i], "|"))
 		stru->type = 2;
 	return (i);
 }
 
-int ft_cd (char **argv)
+void ft_cd(char **argv)
 {
-	int ret = 0, lenght = 0;
-	while (argv[lenght])
-		lenght++;
-	if (lenght < 1)
-		ret = show_error("error: cd: bad arguments\n");
+	int i = 0;
+	while(argv[i])
+		i++;
+	if (i < 1)
+		show_error("error: cd: bad arguments\n");
 	else if (chdir(argv[1]))
 	{
-		ret = show_error("error: cd: cannot change directory to ");
+		show_error("error: cd: cannot change directory to ");
 		show_error(argv[1]);
 		show_error("\n");
 	}
-	return (ret);
 }
 
-int main (int argc, char **argv, char **env)
+int main(int argc, char **argv, char **env)
 {
-	int pipes[2], pipe_open, status, i = 1;
+	int i = 1, pipes[2], status;
 	pid_t pid;
 	t_command stru;
 
@@ -91,34 +90,26 @@ int main (int argc, char **argv, char **env)
 	while (i < argc)
 	{
 		stru.type = 0;
-		pipe_open = 0;
-		i = parse_command(&stru, i);
-		if (stru.type == 2)
-		{
-			pipe_open = 1;
-			if (pipe(pipes))
-				return (exit_fatal());
-		}
+		i = ft_parse_command(&stru, i);
+		if (stru.type == 2 && pipe(pipes))
+			return (fatal_error());
 		pid = fork();
-		if (pid < 0)
-			return (exit_fatal());
 		if (pid == 0)
 		{
-			if (pipe_open)
+			if (stru.type == 2)
 				dup2(pipes[1], 1);
-			if (strcmp(stru.argv[0], "cd") == 0)
+			if (!strcmp(stru.argv[0], "cd"))
 				ft_cd(stru.argv);
-			else if(execve(stru.argv[0], stru.argv, env) < 0)
+			else if ((execve(stru.argv[0], stru.argv, env)) < 0)
 			{
 				show_error("error: cannot execute ");
 				show_error(stru.argv[0]);
 				show_error("\n");
 			}
-			exit(0);
 		}
 		else
 			waitpid(pid, &status, 0);
-		if (pipe_open == 1)
+		if (stru.type == 2)
 		{
 			dup2(pipes[0], 0);
 			close(pipes[1]);
