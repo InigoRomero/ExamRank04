8c6af9f2533040b3e3b16b6564755c8422197be1
Iñigo Romero <45758515+InigoRomero@users.noreply.github.com>
2020-11-12 18:27:17 +0100

    protect system calls

--- microshell.c
+++ microshell.c
@@ -3,15 +3,15 @@
 #include <string.h>
 
 typedef struct s_command{
-	int type;
+	char **argvaux;
 	char **argv;
-	char **argvaux; 
+	int type;
 }				t_command;
 
 int ft_strlen(char *str)
 {
 	int i = 0;
-	while (str[i] != '\0')
+	while (str[i])
 		i++;
 	return (i);
 }
@@ -20,18 +20,18 @@ char *ft_strdup(char *src)
 {
 	char *dest;
 	int i = 0;
-	if (!(dest = malloc(sizeof(char *) * ft_strlen(src) + 1)))
+	if (!(dest = malloc(sizeof(char*) * ft_strlen(src) + 1)))
 		return (NULL);
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[i] = 0;
+	dest[i] = '\0';
 	return (dest);
 }
 
-int show_error(char *str)
+int ft_show_error(char *str)
 {
 	if (str)
 		write(2, str, (size_t)ft_strlen(str));
@@ -40,21 +40,36 @@ int show_error(char *str)
 
 int fatal_error()
 {
-	show_error("error: fatal\n");
+	ft_show_error("fatal: error\n");
 	exit(EXIT_FAILURE);
 	return (EXIT_FAILURE);
+}
+
+void	ft_cd(char **argv)
+{
+	int i = 0;
+	while (argv[i])
+		i++;
+	if (i < 1)
+		ft_show_error("error: cd: bad arguments\n");
+	if (chdir(argv[1]))
+	{
+		ft_show_error("error: cd: cannot change directory to ");
+		ft_show_error(argv[1]);
+		ft_show_error("\n");
+	}
 }
 
 int ft_parse_command(t_command *stru, int i)
 {
 	int x = 0, aux = i;
-	while (stru->argvaux[i] && strcmp(stru->argvaux[i], "|") && strcmp(stru->argvaux[i], ";"))
+	while (stru->argvaux[i] && strcmp(stru->argvaux[i], ";") && strcmp(stru->argvaux[i], "|"))
 		i++;
 	if (!(stru->argv = (char**)malloc(sizeof(char**) * (i - aux + 1))))
-		return (0);
+		return 0;
 	while (x + aux < i)
 	{
-		stru->argv[x] = ft_strdup(stru->argvaux[x + aux]);
+		stru->argv[x] = ft_strdup(stru->argvaux[aux + x]);
 		x++;
 	}
 	stru->argv[x] = 0;
@@ -65,47 +80,37 @@ int ft_parse_command(t_command *stru, int i)
 	return (i);
 }
 
-void ft_cd(char **argv)
-{
-	int i = 0;
-	while(argv[i])
-		i++;
-	if (i < 1)
-		show_error("error: cd: bad arguments\n");
-	else if (chdir(argv[1]))
-	{
-		show_error("error: cd: cannot change directory to ");
-		show_error(argv[1]);
-		show_error("\n");
-	}
-}
-
 int main(int argc, char **argv, char **env)
 {
-	int i = 1, pipes[2], status;
-	pid_t pid;
+	int i = 1, status, x = 0, pipes[2];
 	t_command stru;
+	pid_t pid;
 
 	stru.argvaux = argv;
 	while (i < argc)
 	{
+		x = 0;
 		stru.type = 0;
-		i = ft_parse_command(&stru, i);
+		if ((i = ft_parse_command(&stru, i)) == 0)
+			return (fatal_error());
 		if (stru.type == 2 && pipe(pipes))
 			return (fatal_error());
 		pid = fork();
+		if (pid < 0)
+            fatal_error();
 		if (pid == 0)
 		{
 			if (stru.type == 2)
 				dup2(pipes[1], 1);
 			if (!strcmp(stru.argv[0], "cd"))
 				ft_cd(stru.argv);
-			else if ((execve(stru.argv[0], stru.argv, env)) < 0)
+			else if((execve(stru.argv[0], stru.argv, env)) < 0)
 			{
-				show_error("error: cannot execute ");
-				show_error(stru.argv[0]);
-				show_error("\n");
+				ft_show_error("error: cannot execute ");
+				ft_show_error(stru.argv[0]);
+				ft_show_error("\n");
 			}
+			exit(0);
 		}
 		else
 			waitpid(pid, &status, 0);
@@ -115,7 +120,6 @@ int main(int argc, char **argv, char **env)
 			close(pipes[1]);
 			close(pipes[0]);
 		}
-		int x = 0;
 		while (stru.argv[x])
 			free(stru.argv[x++]);
 		free(stru.argv);
